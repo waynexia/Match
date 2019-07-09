70ce2883b4945f2b31c59fe1fd2de8b8c86cc880
Ruihang Xia <waynestxia@gmail.com>
2019-07-09 23:13:40 +0800

    halfway commit

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,31 @@ extern crate diesel;
 #[macro_use]
 extern crate actix_web;
 
+#[macro_use]
+extern crate serde_derive;
+
 use actix_session::Session;
 use actix_web::http::{Method, StatusCode};
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Result};
+use actix_web::web::{Json,resource,JsonConfig,post};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
 mod models;
 mod schema;
 mod utils;
+
+#[derive(Deserialize,Debug,Serialize)]
+struct UserInfo{
+    nickname: String,
+    email: String,
+    password: String,
+}
+
+fn add_user(info: Json<UserInfo>,req: HttpRequest) -> HttpResponse{
+    println!("{:?}",info);
+    HttpResponse::Ok().json(info.0)
+}
 
 #[post("/api/spider/add_game")]
 fn add_game(session: Session, req: HttpRequest) -> Result<HttpResponse> {
@@ -32,7 +48,17 @@ fn ping(session: Session, req: HttpRequest) -> Result<HttpResponse> {
 }
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(ping).service(add_game))
-        .bind("127.0.0.1:8080")?
-        .run()
+    let conn = utils::establish_connection();
+    HttpServer::new(|| {
+        App::new()
+            .service(ping)
+            .service(add_game)
+            .service(
+                resource("/api/add_user")
+                    .data(JsonConfig::default().limit(1024)) // <- limit size of the payload (resource level)
+                    .route(post().to_async(add_user)),
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
 }
--- src/utils.rs
+++ src/utils.rs
@@ -4,17 +4,18 @@ use diesel::prelude::*;
 pub fn establish_connection() -> MysqlConnection {
     // dotenv.ok();
 
+    // mysql://[user[:password]@]host/database_name
     // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_url = "mysql://localhost/test";
+    let database_url = "mysql://root@localhost/test";
     MysqlConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connect to {}", database_url))
 }
 
 pub fn create_user(conn: &MysqlConnection, nickname: &str, email: &str, password: &str) -> User {
-    use crate::schema::user;
     use crate::models::NewUser;
+    use crate::schema::user;
 
-    let new_user = NewUser{
+    let new_user = NewUser {
         nickname,
         email,
         password,
